2f5c167c3cfb0613fa105a8500b2477b476b8686
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-12-16 18:49:18 +0500

    Project

--- Tyuiu.KarpovAA.Sprint7.Project.V12.Test/DataServiceTest.cs
+++ Tyuiu.KarpovAA.Sprint7.Project.V12.Test/DataServiceTest.cs
@@ -13,7 +13,15 @@ namespace Tyuiu.KarpovAA.Sprint7.Project.V12.Test
         {
             DataService ds = new DataService();
 
+            string path = @"C:\OutPutFile.csv";
 
+            FileInfo fileinfo = new FileInfo(path);
+
+            bool fileex = fileinfo.Exists;
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileex);
         }
     }
 }
--- Tyuiu.KarpovAA.Sprint7.Project.V12/FormMain.Designer.cs
+++ Tyuiu.KarpovAA.Sprint7.Project.V12/FormMain.Designer.cs
@@ -29,10 +29,17 @@ namespace Tyuiu.KarpovAA.Sprint7.Project.V12
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormMain";
+            this.Text = "Спринт 7 | Task Project | Вариант 12 | Карпов А. А.";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
